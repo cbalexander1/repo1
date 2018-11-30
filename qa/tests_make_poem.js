eea7a66a42be51589311a1b45d6dc57ddb24cd1a
cbalexander1 <cbalexander1@agora.cs.wcu.edu>
2018-11-29 23:02:33 -0500

    The test files for data structures and make poem

---
+++ qa/tests_make_poem.js
@@ -0,0 +1,40 @@
+/**                                                                             
+ * @author Clayton Alexander                                                    
+ * @version 1.0                                                                 
+ *                                                                              
+ * Third project for cs253. This file holds the tests for make_poem.js.                   
+ *                                                                              
+ */                                                                             
+                                                                                
+//var assert = require('chai');                                                 
+var expect = require('chai');                                                   
+var exports = require('../data_structures.js');                                 
+                                                                                
+/**A test suite for testing the object**/                                       
+describe("Tests", function(){                                                   
+    //Test created for the whole suite                                          
+                                                                                
+                                                                                
+    /**Called when the suite begins to set up objects for other tests**/        
+    before(function(){                                                          
+    });                                                                         
+                                                                                
+    /**Called before each unit test **/                                         
+    beforeEach(function(){                                                      
+    });                                                                         
+                                                                                
+    /**Called after each function to reset objects **/                          
+    afterEach(function(){                                                       
+    });                                                                         
+                                                                                
+    /**Called as the test suite ends**/                                         
+    after(function(){                                                           
+    });                                                                         
+                                                                                
+    /**The Unit Tests**/                                                        
+                                                                                
+    describe('Unit tests for function', function(){                   
+                                                                                
+    });                                                                         
+                                                                                
+});                  
